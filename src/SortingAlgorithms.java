c4b344c1a7bea4175b19aebb46bb4e4a63a84010
gujoh <gustafjohanson@live.com>
2020-11-21 20:10:10 +0100

    Adding a swap method.

--- src/SortingAlgorithms.java
+++ src/SortingAlgorithms.java
@@ -20,30 +20,31 @@ public class SortingAlgorithms {
         }
     }
 
+    public void swap (int[] arr, int x, int y){
+        int temp = arr[x];
+        arr[x] = arr[y];
+        arr[y] = temp;
+    }
+
+
     //SORTING ALGORITHMS START HERE//
 
     public void bubbleSort (int[] arr){
         for (int i = 0; i < arr.length; i++) {
             for (int j = 1; j < arr.length - i; j++) {
-                if (arr[j - 1] > arr[j]) {
-                    int temp = arr[j];
-                    arr[j] = arr[j - 1];
-                    arr[j - 1] = temp;
-                }
+                if (arr[j - 1] > arr[j])
+                    swap(arr, j, j - 1);
             }
         }
     }
 
-    public void selectionSort(int[] arr){                  //Is this actually a selection sort?
-        for (int i = 0; i < arr.length; i++) {              //Or is selectionSort2() better despite being a little longer?
-            for (int j = i + 1; j < arr.length; j++) {      //This method swaps the value of index i in the array
-                if (arr[j] < arr[i]) {                      //as soon as it finds a smaller value to the right of it.
-                    int temp = arr[j];                      //selectionSort2() on the other hand only swaps index i
-                    arr[j] = arr[i];                        //when it has traversed the whole array to the right of i
-                    arr[i] = temp;                          //and found a value smaller than i.
-                }
-            }
-        }
+    public void selectionSort(int[] arr){                       //Is this actually a selection sort?
+        for (int i = 0; i < arr.length; i++) {                  //Or is selectionSort2() better despite being a little longer?
+            for (int j = i + 1; j < arr.length; j++) {          //This method swaps the value of index i in the array
+                if (arr[j] < arr[i])                            //as soon as it finds a smaller value to the right of it.
+                    swap(arr, j, i);                           //selectionSort2() on the other hand only swaps index i
+            }                                                   //when it has traversed the whole array to the right of i
+        }                                                       //and found a value smaller than i.
     }
 
     public void selectionSort2(int[] arr){
@@ -63,18 +64,12 @@ public class SortingAlgorithms {
     public void cocktailShakerSort(int[] arr){
         for (int i = 0; i < arr.length; i++) {
             for (int j = 1; j < arr.length - i; j++) {
-                if (arr[j - 1] > arr[j]) {
-                    int temp = arr[j];
-                    arr[j] = arr[j - 1];
-                    arr[j - 1] = temp;
-                }
+                if (arr[j - 1] > arr[j])
+                    swap(arr, j, j - 1);
             }
             for (int k = arr.length - i - 1; k > 0; k--) {      //Just add this for-loop to a bubble sort, and
-                if (arr[k] < arr[k - 1]){                       //you've got a cocktail shaker sort.
-                    int temp = arr[k];
-                    arr[k] = arr[k - 1];
-                    arr[k - 1] = temp;
-                }
+                if (arr[k] < arr[k - 1])                        //you've got a cocktail shaker sort.
+                    swap(arr, k, k - 1);
             }
         }
     }
@@ -82,11 +77,8 @@ public class SortingAlgorithms {
     public void insertionSort(int[] arr){
         for (int i = 0; i < arr.length; i++) {
             for (int j = i; j > 0 ; j--) {
-                if (arr[j] < arr[j - 1]) {
-                    int temp = arr[j];
-                    arr[j] = arr[j - 1];
-                    arr[j - 1] = temp;
-                }
+                if (arr[j] < arr[j - 1])
+                    swap(arr, j, j  - 1);
             }
         }
     }
